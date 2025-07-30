f5538277e950dd7ef05b60af48b48ad9397d811e
Moyinoluwalogo Oladipo <m.oladips@gmail.com>
2025-07-31 00:15:26 +0100

    - Created a MongoDB Database for Vaxitrack. - Connected the Database to the server.js. - Installed mongoose as a dependency to manage connections to the DB. - Installed dotenv to handle secret variables. - Created the Models folder to handles models within the Database. - Created the first model, User.js. - Also created the config folde to hold db.js that handles the Database   connection.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import adminRouter from './routes/adminRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
 
+const PORT = process.env.PORT || 7000;
 const app = express(); 
+dotenv.config();
+
+connectDB();
 
 app.use("/vaxitrack/admin", adminRouter);
 app.use("/vaxitrack/user", userRouter);
 
-app.listen(7000 , () => {
-  console.log('Vaxitrack Server is running on port 7000');
+app.listen(PORT , () => {
+  console.log("Vaxitrack Server is running on PORT:", PORT);
 });
+
